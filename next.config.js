1db73bac91fec3c7223df079550dbec1bd59f23c
User <user@example.com>
2026-05-28 20:57:53 +0200

    Reverse proxy: Rewrite all requests to TikTok

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.tiktok.com/:path*',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
